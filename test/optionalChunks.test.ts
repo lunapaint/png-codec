c83e7f6e7fb8f2e6435718297a4c90834ec587fd
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-01-08 15:39:34 -0800

    Optional chunks tests

---
+++ test/optionalChunks.test.ts
@@ -0,0 +1,25 @@
+import { deepStrictEqual } from 'assert';
+import { join, resolve } from 'path';
+import { decodePng } from '../out/png.js';
+import * as fs from 'fs';
+import { KnownChunkTypes } from '../out/types.js';
+
+const pngSuiteRoot = 'test/pngsuite/png';
+
+describe('pngParser.optionalChunks', () => {
+  it('should not load sBIT when not specified', async () => {
+    const data = new Uint8Array(await fs.promises.readFile(join(pngSuiteRoot, `cs3n2c16.png`)));
+    const result = await decodePng(data);
+    deepStrictEqual(result.metadata, undefined);
+  });
+  it('should load sBIT when specified', async () => {
+    const data = new Uint8Array(await fs.promises.readFile(join(pngSuiteRoot, `cs3n2c16.png`)));
+    const result = await decodePng(data, { parseChunkTypes: [KnownChunkTypes.sBIT] });
+    deepStrictEqual(result.metadata, [
+      {
+        type: 'sBIT',
+        value: [13, 13, 13]
+      }
+    ]);
+  });
+});
