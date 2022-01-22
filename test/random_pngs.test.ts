2faa02163e8d25b54c82df9826ea855cff248361
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-01-22 08:11:18 -0800

    Setup basic random_pngs testing

---
+++ test/random_pngs.test.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright (c) 2022 Daniel Imms <http://www.growingwiththeweb.com>
+ * Released under MIT license. See LICENSE in the project root for details.
+ */
+
+import * as fs from 'fs';
+import { createTests } from './testUtil.js';
+
+const suiteRoot = 'test/random_pngs';
+
+function getNumberAtIndex(file: string, index: number): number {
+  let text = '^rand';
+  for (let i = 0; i < 7; i++) {
+    text += '_';
+    if (i === index) text += '(';
+    text += '[0-9]+';
+    if (i === index) text += ')';
+  }
+  const regex = new RegExp(text);
+  return parseInt(file.match(regex)![1]);
+}
+
+function colorTypeIdToName(id: 0 | 2 | 3 | 4 | 6): string {
+  switch (id) {
+    case 0: return 'greyscale';
+    case 2: return 'truecolor';
+    case 3: return 'indexed';
+    case 4: return 'greyscale and alpha';
+    case 6: return 'truecolor and alpha';
+  }
+}
+
+// The random_pngs repo is a collection of generated lodepng encoded pngs for fuzzing purposed. The
+// pixel data is random and not being tested, only ensuring images load in strict mode and have the
+// expected properties.
+describe('random_pngs', () => {
+  const testFiles = fs.readdirSync(suiteRoot);
+  const testsByColorType: Map<number, string[]> = new Map();
+  const colorTypes: (0 | 2 | 3 | 4 | 6)[] = [0, 2, 3, 4, 6];
+  for (const colorType of colorTypes) {
+    testsByColorType.set(colorType, []);
+  }
+  for (const file of testFiles) {
+    if (file.endsWith('png')) {
+      const colorType = getNumberAtIndex(file, 3);
+      testsByColorType.get(colorType)!.push(file.replace('.png', ''));
+    }
+  }
+  for (const colorType of colorTypes) {
+    describe(`Color type ${colorTypeIdToName(colorType)} (${colorType})`, () => {
+      const colorTypeCases = testsByColorType.get(colorType)!;
+      for (const file of colorTypeCases) {
+        createTests([
+          [file, colorType.toString(), { strictMode: true, skipDataAssertion: true, expectedDimensions: { width: 1, height: 1 } }],
+        ], suiteRoot);
+      }
+    });
+  }
+});
