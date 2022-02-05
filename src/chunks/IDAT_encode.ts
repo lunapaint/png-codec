eae5f4d16657a9c056273789ed9f72e9f5be44ee
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-02-05 09:09:55 -0800

    Up and sub filter encoding

--- src/chunks/IDAT_encode.ts
+++ src/chunks/IDAT_encode.ts
@@ -66,6 +66,10 @@ function writeUncompressedData(
   image: Readonly<IImage32> | Readonly<IImage64>,
   stream: ByteStream
 ) {
+  // TODO: Support filtering:
+  // - If the image type is Palette, or the bit depth is smaller than 8, then do not filter the image (i.e. use fixed filtering, with the filter None).
+  // - (The other case) If the image type is Grayscale or RGB (with or without Alpha), and the bit depth is not smaller than 8, then use adaptive filtering as follows: independently for each row, apply all five filters and select the filter that produces the smallest sum of absolute values per row.
+
   let y = 0;
   let x = 0;
   let i = 0;
@@ -91,22 +95,75 @@ function writeUncompressedData(
     }
     case ColorType.Truecolor: {
       for (; y < image.height; y++) {
+        const filterType = pickFilterType(image, stream.offset, stream);
         // Filter type
-        stream.writeUint8(0);
+        stream.writeUint8(filterType);
 
         // Data
-        for (x = 0; x < image.width; x++) {
-          if (ctx.bitDepth === 16) {
-            stream.writeUint16(image.data[i++]);
-            stream.writeUint16(image.data[i++]);
-            stream.writeUint16(image.data[i++]);
-            i++;
-          } else {
-            stream.writeUint8(image.data[i++]);
-            stream.writeUint8(image.data[i++]);
-            stream.writeUint8(image.data[i++]);
-            i++;
-          }
+        switch (filterType) {
+          case FilterType.None:
+            for (x = 0; x < image.width; x++) {
+              if (ctx.bitDepth === 16) {
+                stream.writeUint16(image.data[i++]);
+                stream.writeUint16(image.data[i++]);
+                stream.writeUint16(image.data[i++]);
+                i++;
+              } else {
+                stream.writeUint8(image.data[i++]);
+                stream.writeUint8(image.data[i++]);
+                stream.writeUint8(image.data[i++]);
+                i++;
+              }
+            }
+            break;
+          case FilterType.Sub:
+            if (ctx.bitDepth === 16) {
+              throw new Error('Sub not supported yet for 16 bit');
+              // stream.writeUint16(image.data[i++]);
+              // stream.writeUint16(image.data[i++]);
+              // stream.writeUint16(image.data[i++]);
+              // i++;
+            } else {
+              stream.writeUint8(image.data[i++]);
+              stream.writeUint8(image.data[i++]);
+              stream.writeUint8(image.data[i++]);
+              i++;
+            }
+            for (x = 1; x < image.width; x++) {
+              // if (ctx.bitDepth === 16) {
+              //   stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 6)) % 256);
+              //   stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 4)) % 256);
+              //   stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 2)) % 256);
+              //   i++;
+              // } else {
+              stream.writeUint8((image.data[i    ] - image.data[i     - 4] + 256) % 256);
+              stream.writeUint8((image.data[i + 1] - image.data[i + 1 - 4] + 256) % 256);
+              stream.writeUint8((image.data[i + 2] - image.data[i + 2 - 4] + 256) % 256);
+              i += 4;
+              // }
+            }
+            break;
+          case FilterType.Up:
+            if (y === 0) {
+              throw new Error('Cannot encode with filter type Up on first line');
+            }
+            for (x = 0; x < image.width; x++) {
+              if (ctx.bitDepth === 16) {
+                throw new Error('Sub not supported yet for 16 bit');
+                // stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 6)) % 256);
+                // stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 4)) % 256);
+                // stream.writeUint16((image.data[i++] - stream.view.getUint16(stream.offset - 2)) % 256);
+                // i++;
+              } else {
+                stream.writeUint8((image.data[i    ] - image.data[i     - image.width * 4] + 256) % 256);
+                stream.writeUint8((image.data[i + 1] - image.data[i + 1 - image.width * 4] + 256) % 256);
+                stream.writeUint8((image.data[i + 2] - image.data[i + 2 - image.width * 4] + 256) % 256);
+                i += 4;
+              }
+            }
+            break;
+          default:
+            throw new Error('Only none and sub filter types are supported');
         }
       }
       break;
@@ -116,7 +173,7 @@ function writeUncompressedData(
         throw new Error('Cannot encode indexed file without palette');
       }
       for (; y < image.height; y++) {
-        // Filter type
+        // Filter type - indexed images always use no filter intentionally
         stream.writeUint8(0);
 
         // Data
@@ -186,4 +243,113 @@ function writeUncompressedData(
     default:
       throw new Error(`Color type "${ctx.colorType}" not supported yet`);
   }
+}
+
+function pickFilterType(
+  image: Readonly<IImage32> | Readonly<IImage64>,
+  lineIndex: number,
+  stream: ByteStream
+): FilterType {
+  if (image.data.BYTES_PER_ELEMENT === 2) {
+    console.warn('16 bit images can\'t select filters yet');
+    return FilterType.None;
+  }
+  const filterSums: Map<FilterType, number> = new Map();
+  for (const filterType of [0, 1, 2, 3, 4] as FilterType[]) {
+    let sum = 0;
+    switch (filterType) {
+      case FilterType.None:
+        for (let i = lineIndex; i < lineIndex + image.width * 4; i += 4) {
+          // TODO: This is for truecolor, handle other color types
+          sum += (
+            image.data[i    ] +
+            image.data[i + 1] +
+            image.data[i + 2]
+          );
+        }
+      case FilterType.Sub:
+        // TODO: This is only for truecolor, handle other color types
+        sum += (
+          image.data[lineIndex    ] +
+          image.data[lineIndex + 1] +
+          image.data[lineIndex + 2]
+        );
+        for (let i = lineIndex + 4; i < lineIndex + image.width * 4; i += 4) {
+          sum += (
+            (image.data[i    ] - image.data[i     - 4] + 256) % 256 +
+            (image.data[i + 1] - image.data[i + 1 - 4] + 256) % 256 +
+            (image.data[i + 2] - image.data[i + 2 - 4] + 256) % 256
+          );
+        }
+        break;
+      case FilterType.Up:
+        // The first line should not use up as it's essentially just None
+        if (lineIndex === 0) {
+          sum = Infinity;
+        } else {
+          for (let i = lineIndex; i < lineIndex + image.width * 4; i += 4) {
+            // Add 256 to ensure it's positive for modulo
+            sum += (
+              ((image.data[i    ] - image.data[i     - image.width * 4] + 256) % 256) +
+              ((image.data[i + 1] - image.data[i + 1 - image.width * 4] + 256) % 256) +
+              ((image.data[i + 2] - image.data[i + 2 - image.width * 4] + 256) % 256)
+            );
+          }
+        }
+        break;
+      default:
+        sum = Infinity;
+    }
+    filterSums.set(filterType, sum);
+  }
+  let lowestFilterType: FilterType = FilterType.None;
+  let lowestSum = filterSums.get(FilterType.None)!;
+  for (const filterType of [1, 2, 3, 4] as FilterType[]) {
+    if (filterSums.get(filterType)! < lowestSum) {
+      lowestFilterType = filterType;
+      lowestSum = filterSums.get(filterType)!;
+    }
+  }
+  console.log('filter sums', Array.from(filterSums.values()));
+  console.log('lowest filter type', lowestFilterType);
+  return lowestFilterType;
+}
+
+// TODO: Duplicated in decode file
+const enum FilterType {
+  /**
+   * ```
+   * Filt(x) = Orig(x)
+   * Recon(x) = Filt(x)
+   * ```
+   */
+  None = 0,
+  /**
+   * ```
+   * Filt(x) = Orig(x) - Orig(a)
+   * Recon(x) = Filt(x) + Recon(a)
+   * ```
+   */
+  Sub = 1,
+  /**
+   * ```
+   * Filt(x) = Orig(x) - Orig(b)
+   * Recon(x) = Filt(x) + Recon(b)
+   * ```
+   */
+  Up = 2,
+  /**
+   * ```
+   * Filt(x) = Orig(x) - floor((Orig(a) + Orig(b)) / 2)
+   * Recon(x) = Filt(x) + floor((Recon(a) + Recon(b)) / 2)
+   * ```
+   */
+  Average = 3,
+  /**
+   * ```
+   * Filt(x) = Orig(x) - PaethPredictor(Orig(a), Orig(b), Orig(c))
+   * Recon(x) = Filt(x) + PaethPredictor(Recon(a), Recon(b), Recon(c))
+   * ```
+   */
+  Paeth = 4
 }
